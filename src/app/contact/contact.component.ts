bf8638331446f233eb1af3dcff4b141d8d2ced93
latifbey <latifgungor51@gmail.com>
2018-01-28 18:57:59 +0100

    3.odev bitmedi ama hatasiz consol compile

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,65 +9,65 @@ import { Feedback, ContactType } from '../shared/feedback';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  
-    feedbackForm: FormGroup;
-    feedback: Feedback;
-    contactType = ContactType;
-    formErrors = {
-      'firstname': '',
-      'lastname': '',
-      'telnum': '',
-      'email': ''
-    };
-  
-    validationMessages = {
-      'firstname': {
-        'required':      'First Name is required.',
-        'minlength':     'First Name must be at least 2 characters long.',
-        'maxlength':     'FirstName cannot be more than 25 characters long.'
-      },
-      'lastname': {
-        'required':      'Last Name is required.',
-        'minlength':     'Last Name must be at least 2 characters long.',
-        'maxlength':     'Last Name cannot be more than 25 characters long.'
-      },
-      'telnum': {
-        'required':      'Tel. number is required.',
-        'pattern':       'Tel. number must contain only numbers.'
-      },
-      'email': {
-        'required':      'Email is required.',
-        'email':         'Email not in valid format.'
-      },
-    };
-  
-  
-    constructor(private fb: FormBuilder) {
-      this.createForm();
-    }
-  
-    ngOnInit() {
-    }
-  
-    createForm(): void {
-      this.feedbackForm = this.fb.group({
-        firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-        lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-        telnum: ['', [Validators.required, Validators.pattern] ],
-        email: ['', [Validators.required, Validators.email] ],
-        agree: false,
-        contacttype: 'None',
-        message: ''
-      });
-    }
+
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required': 'First Name is required.',
+      'minlength': 'First Name must be at least 2 characters long.',
+      'maxlength': 'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required': 'Last Name is required.',
+      'minlength': 'Last Name must be at least 2 characters long.',
+      'maxlength': 'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required': 'Tel. number is required.',
+      'pattern': 'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'email': 'Email not in valid format.'
+    },
+  };
+
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
-    this.feedbackForm.valueChanges
-    .subscribe(data => this.onValueChanged(data));
 
-  this.onValueChanged(); // (re)set validation messages now
+  ngOnInit() {
+  }
 
+  createForm(): void {
+    this.feedbackForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  
 
-onValueChanged(data? : any) {
+ this.feedbackForm.valueChanges
+  .subscribe(data => this.onValueChanged(data));
+
+this.onValueChanged(); // (re)set validation messages now
+}
+
+onValueChanged(data ? : any) {
   if (!this.feedbackForm) { return; }
   const form = this.feedbackForm;
   for (const field in this.formErrors) {
@@ -82,18 +82,19 @@ onValueChanged(data? : any) {
     }
   }
 }
-  
-    onSubmit() {
-      this.feedback = this.feedbackForm.value;
-      console.log(this.feedback);
-      this.feedbackForm.reset({
-        firstname: '',
-        lastname: '',
-        telnum: '',
-        email: '',
-        agree: false,
-        contacttype: 'None',
-        message: ''
-      });
-    }
-  
+
+onSubmit() {
+  this.feedback = this.feedbackForm.value;
+  console.log(this.feedback);
+  this.feedbackForm.reset({
+    firstname: '',
+    lastname: '',
+    telnum: '',
+    email: '',
+    agree: false,
+    contacttype: 'None',
+    message: ''
+  });
+}
+
+}
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { comments } from '../shared/comments';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Feedback } from '../shared/feedback';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -17,31 +20,96 @@ import { Location } from '@angular/common';
   styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
+
+  dish: Dish;
+  dishIds: number[];
+  prev: number;
+  next: number;
+  comments: comments[];
+  feedbackForm: FormGroup;
+  feedback: Feedback;
   
-    dish: Dish;
-    dishIds: number[];
-    prev: number;
-    next: number;
-    comments: comments[];
-    constructor(private dishservice: DishService,
-      private route: ActivatedRoute,
-      private location: Location) { }
+  formErrors = {
+    'name': '',
+    'lastname': '',
+   
+  };
+
+  validationMessages = {
+    'name': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 2 characters long.',
+      'maxlength': 'Name cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required': 'Last Name is required.',
+      'minlength': 'Last Name must be at least 2 characters long.',
+      'maxlength': 'Last Name cannot be more than 25 characters long.'
+    }
+    
+  };
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    };
+
+    createForm(): void {
+      this.feedbackForm = this.fb.group({
+        name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+        lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]]
+       
+      });
+    
   
-    ngOnInit() {
-      
-          this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-          this.route.params
-            .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-            .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+   this.feedbackForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+  
+  this.onValueChanged(); // (re)set validation messages now
+  }
+  
+  onValueChanged(data ? : any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
         }
-      
-        setPrevNext(dishId: number) {
-          let index = this.dishIds.indexOf(dishId);
-          this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
-          this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
-        }
-  
-    goBack(): void {
-      this.location.back();
+      }
     }
   }
+  
+  onSubmit() {
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset({
+      name: '',
+      lastname: ''
+     
+    });
+  }
+
+  ngOnInit() {
+
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  setPrevNext(dishId: number) {
+    let index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
